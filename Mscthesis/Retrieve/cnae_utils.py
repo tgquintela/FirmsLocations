8d3da2d826eaff38fcf1f61b311e7638c81aac71
antonio <tgq.spm@gmail.com>
2015-04-08 06:59:31 +0200

    Fixing cnae utilities.

--- Mscthesis/Retrieve/cnae_utils.py
+++ Mscthesis/Retrieve/cnae_utils.py
@@ -6,6 +6,5 @@ Module oriented to retrieve columns related with cnae index.
 
 def retrieve_cols(df, val, cols):
     lvl = len(val)
-    res = df[df[df['cnae'].apply(lambda x: x[:lvl])] == val]
-    res = res[cols]
+    res = df[cols][df['cnae'].apply(lambda x: x[:lvl]) == val]
     return res
