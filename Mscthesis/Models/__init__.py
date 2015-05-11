9469f5215aa6e83dca22bf39ca58da7aca62da29
antonio <tgq.spm@gmail.com>
2015-05-11 02:42:42 +0200

    Correcting Model class

--- Mscthesis/Models/__init__.py
+++ Mscthesis/Models/__init__.py
@@ -8,10 +8,14 @@ from model_utils import filter_with_random_nets
 from Mscthesis.IO.model_report import create_model_report
 import networkx as nx
 from os.path import join
+import shelve
 
 
 class Model():
     """Abstract class for all the models with common utilities."""
+
+    time_expended = 0.
+
     def filter_with_random_nets(self, nets, p_thr):
         "Filter non-significant weiths."
         net, random_nets = nets[:, :, 0], nets[:, :, 1:]
@@ -24,6 +28,18 @@ class Model():
         return fig1, fig2
 
     def to_pajek(self, net, sectors, netfiledata, filenamenet):
+        "Export net to pajek format *.net"
         net_out = nx.from_numpy_matrix(net)
-        net_out = nx.relabel_nodes(net_out, dict(zip(range(len(sectors)), sectors)))
+        n_sects = len(sectors)
+        net_out = nx.relabel_nodes(net_out, dict(zip(range(n_sects), sectors)))
         nx.write_pajek(net_out, join(netfiledata, filenamenet))
+
+    def save_net_to_file(self, net, sectors, N_t, N_x, outputfile):
+        "Save interesting quantities in a external file."
+        database = shelve.open(outputfile)
+        database['net'] = net
+        database['sectors'] = sectors
+        database['time'] = self.time_expended
+        database['N_t'] = N_t
+        database['N_x'] = N_x
+        database.close()
