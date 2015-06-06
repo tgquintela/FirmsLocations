32dbb23bb0d2b1a688cc91b8dfa24c08f523f038
antonio <tgq.spm@gmail.com>
2015-06-06 18:54:42 +0200

    Correcting preparation module.

--- Mscthesis/IO/preparation_module.py
+++ Mscthesis/IO/preparation_module.py
@@ -11,11 +11,13 @@ def prepare_filterinfo(year):
     loc_vars = ['ES-X', 'ES-Y']
     if year is None:
         years = [2006, 2007, 2008, 2009, 2010, 2011, 2012]
+        all_y = False
     else:
         years = [year]
+        all_y = True
     # Creation dicts
     date_filter_nfo = {'method': 'by_years',
-                       'date_filter_nfo': {'years': years}}
+                       'date_filter_nfo': {'years': years, 'all_y': all_y}}
     coord_filter_nfo = {'coord_vars': loc_vars, 'method': 'spaincoordinates'}
     filterinfo = {'coord_filter_nfo': coord_filter_nfo,
                   'date_filter_nfo': date_filter_nfo}
