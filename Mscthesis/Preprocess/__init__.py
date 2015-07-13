9d0a1aea51abaabc6bf03eba5f1867e783af3d79
tgquintela <tgquintela@github.com>
2015-07-13 23:28:15 +0200

    Initial commit for separation modules.

--- Mscthesis/Preprocess/__init__.py
+++ Mscthesis/Preprocess/__init__.py
@@ -7,20 +7,21 @@ processes we want to perform.
 
 TODO:
 -----
-
+- Reverse process
+- Processor class
 """
 
 import numpy as np
 import pandas as pd
 from itertools import product
-#from Mscthesis.IO.io_aggfile import read_aggregation
-from comp_complementary_data import compute_aggregate_counts,\
-    average_position_by_aggarr
+
 from preprocess_cols import generate_replace, transform_cnae_col
-from Mscthesis.Geo_tools.geo_transformations import general_projection
+
+from pySpatialTools.Geo_tools.geo_transformations import general_projection
+from pythonUtils.ProcessTools import Processer
 
 
-class Firms_Preprocessor():
+class Firms_Preprocessor(Processor):
     "Special class to preprocess firms data."
     projection_values = None
     map_vars = None
@@ -28,12 +29,18 @@ class Firms_Preprocessor():
 
     ## TODO: map indices
 
-    def __init__(self, typevars):
+    def __init__(self, typevars, logfile, ):
         self.typevars = typevars
         self.map_vars = ['cnae', 'cp']
+        ## General process parameters
+        self.proc_name = "Preprocess empresas"
+        self.proc_desc = "Preprocessing data in order to be treatable"
+        #self.subproc_desc = ["Preprocess manufacturas", "Cleaning servicios"]
+        #self.t_expended_subproc = [0, 0]
 
     def preprocess(self, empresas, cnae_lvl=2, method_proj='ellipsoidal', radians=False):
         "Function to preprocess firms data."
+
         ## 0. Set vars
         finantial_vars = [e for e in self.typevars['feat_vars'] if e != 'cnae']
         loc_vars = self.typevars['loc_vars']
@@ -47,25 +54,17 @@ class Firms_Preprocessor():
                                                 inverse=False,
                                                 radians=radians)
         ## 3. Feature array
-        # TODO: temporal line (filter of none values of cp)
-        #logi = empresas['cp'].isnull()
-        #logi = np.logical_not(logi)
-        #empresas = empresas.loc[logi, :]
-        #################################
         # generate replacement in discrete vars
         t_vals = {'cnae': sorted(list(empresas['cnae'].unique())),
                   'cp': sorted(list(empresas['cp'].unique()))}
         self.map_info = generate_replace(t_vals)
         # cnae variable
         empresas.loc[:, 'cnae'] = transform_cnae_col(empresas['cnae'], cnae_lvl)
-        #empresas['cnae'] = empresas['cnae'].replace(self.map_vars['cnae']).astype(int)
-        # cp variable
-        #empresas.loc[:, 'cp'] = empresas.loc[:, 'cp'].replace(self.map_vars['cp']).astype(int)
         # Map discrete variables
         mpvars = self.map_vars
         empresas.loc[:, mpvars] = empresas.loc[:, mpvars].replace(self.map_info).astype(int)
         # Finantial variables
-
+        ### TODO
         return empresas
 
     def reverse_preprocess(self, empresas):
@@ -78,123 +77,3 @@ class Firms_Preprocessor():
         ##TODO
         return empresas
 
-
-class Aggregator:
-    "Aggregate or read aggregate information."
-
-    def __init__(self, filepath=None, typevars=None):
-        """It is needed the identification of the aggvar and feat_vars with the
-        filepath if we want to get a aggregation from a file.
-        """
-        self.typevars = format_typevars(typevars)
-        if filepath is None:
-            self.bool_read_agg = False
-        else:
-            self.bool_read_agg = True
-            self.filepath = filepath
-
-    def retrieve_aggregation(self, df=None, reindices=None, funct=None):
-        "Main function for retrieving aggregation."
-        if self.bool_read_agg:
-            # TODO: Function to read file
-            filepath, typevars = self.filepath, self.typevars
-            agglocs, aggfeatures = read_aggregation(filepath, typevars)
-        else:
-            ## Correct inputs
-            #################
-            locs = df[self.typevars['loc_vars']].as_matrix()
-            feat_arr = df[self.typevars['feat_vars']].as_matrix()
-            agg_arr = df[self.typevars['agg_var']].as_matrix()
-#            if self.typevars['agg_var'] is None:
-#                ## Check if discretized or to do
-#                if type(self.discretizor) == np.ndarray:
-#                    agg_arr = self.discretizor
-#                    agglocs = average_position_by_aggarr(locs, agg_arr)
-#                else:
-#                    agg_arr = self.discretizor.map2id(locs)
-#                    agglocs = self.discretizor.discretize(locs)
-#                self.typevars['agg_var'] = 'aggvar'
-#            else:
-#                agg_arr = df[self.typevars['agg_var']].as_matrix()
-#                agglocs = average_position_by_aggarr(locs, agg_arr)
-            if reindices is None:
-                N_t = locs.shape[0]
-                reindices = np.array(range(N_t)).reshape((N_t, 1))
-            if len(feat_arr.shape) == 1:
-                feat_arr = feat_arr.reshape(feat_arr.shape[0], 1)
-            ######################################################
-            ## Compute agglocs
-            agglocs = average_position_by_aggarr(locs, agg_arr)
-            ## Compute aggfeatures
-            aggfeatures = create_aggregation(agg_arr, feat_arr, reindices,
-                                             self.typevars, funct)
-        ## Format output
-        agglocs = np.array(agglocs)
-        ndim, N_t = len(agglocs.shape), agglocs.shape[0]
-        agglocs = agglocs if ndim > 1 else agglocs.reshape((N_t, 1))
-        return agglocs, aggfeatures
-
-#    def retrieve_aggloc(self, point_i):
-#        point_i = point_i.reshape(1, 2)
-#        region = self.spatial_disc.map2id(point_i)
-#        return region
-#
-#    def retrieve_agglocs(self, locs):
-#        regions = self.spatial_disc.map2id(locs)
-#        return regions
-
-
-def create_aggregation(agg_arr, feat_arr, reindices, typevars=None,
-                       funct=None):
-    "Create aggregation."
-
-    ## 0. Formatting inputs
-    typevars = format_typevars(typevars, feats_dim=feat_arr.shape[1])
-    feat_vars, agg_var = typevars['feat_vars'], typevars['agg_var']
-    df1 = pd.DataFrame(agg_arr, columns=[agg_var])
-    df2 = pd.DataFrame(feat_arr, columns=feat_vars)
-    df = pd.concat([df1, df2], axis=1)
-
-    ## 1. Use specific function or default aggregate counts
-    if funct is None:
-        agg_desc, _ = compute_aggregate_counts(df, agg_var, feat_vars,
-                                               reindices)
-    else:
-        agg_desc = funct(df, agg_var, feat_vars, reindices)
-    return agg_desc
-
-
-def format_typevars(typevars, locs_dim=None, feats_dim=None):
-    "Check typevars."
-    if typevars is None:
-        typevars = {'agg_var': 'agg'}
-        if locs_dim is not None:
-            loc_vars = [chr(97+i) for i in range(locs_dim)]
-            typevars['loc_vars'] = loc_vars
-        if feats_dim is not None:
-            feat_vars = [str(i) for i in range(feats_dim)]
-            typevars['feat_vars'] = feat_vars
-    if 'agg_var' not in typevars.keys():
-        typevars['agg_var'] = None
-    return typevars
-
-
-def map_multivars2key(multi, vals=None):
-    "Maps a multivariate discrete array to a integer."
-    n_dim, N_t = len(multi.shape), multi.shape[0]
-    if vals is None:
-        vals = []
-        for i in range(n_dim):
-            aux = np.unique(multi[:, i])
-            vals.append(aux)
-    combs = product(*vals)
-    map_arr = -1*np.ones(N_t)
-    i = 0
-    for c in combs:
-        logi = np.ones(N_t).astype(bool)
-        for j in range(n_dim):
-            logi = np.logical_and(logi, multi[:, j] == c[j])
-        map_arr[logi] = i
-        i += 1
-    map_arr = map_arr.astype(int)
-    return map_arr
